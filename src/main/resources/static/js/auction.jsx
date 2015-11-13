9c1ccac4c3d5e24af0f086b7e193a1b611be5a05
Andrew Keely <andrew.keely@gmail.com>
2015-11-12 22:17:10 -0500

    Adding remaining table columns. Adding better polling of server.

--- src/main/resources/static/js/auction.jsx
+++ src/main/resources/static/js/auction.jsx
@@ -1,11 +1,16 @@
 var App = React.createClass({
+    loadAuctionBoard: function () {
+        $.ajax('/api/league/1/bid').done(response => {
+         this.setState({auctionPlayers: response});
+        });
+    },
+
     getInitialState: function () {
         return ({auctionPlayers: []});
     },
     componentDidMount: function () {
-        $.ajax('/api/league/1/bid').done(response => {
-            this.setState({auctionPlayers: response});
-        });
+        this.loadAuctionBoard();
+        // TODO: setInterval(this.loadAuctionBoard, this.props.pollInterval);
     },
     render: function () {
         return (
@@ -42,18 +47,30 @@ var AuctionBoard = React.createClass({
 var Bid = React.createClass({
     render: function () {
         return (
-            <tr>
+            <tr id="bid.{this.props.bid.player.id}">
                 <td>{this.props.bid.player.name}</td>
-                <td>{this.props.bid.team}</td>
+                <td>{this.props.bid.player.positions
+                        .map(function(pos){return pos.name;})
+                        .join(', ')}
+                </td>
                 <td>{this.props.bid.amount}</td>
+                <td>{this.props.bid.team}</td>
+                <td>{this.props.bid.expirationTime}</td>
+                <td width="110">
+                    <div className="input-group input-group-sm">
+                        <input type="text" className="form-control" aria-label="Bid" />
+                        <div className="input-group-btn">
+                            <button type="button" className="btn btn-default" aria-label="Bid">Bid</button>
+                        </div>
+                    </div>
+                </td>
             </tr>
         )
     }
 });
 
-setInterval(function() {
-  ReactDOM.render(
-    <App />,
+ReactDOM.render(
+    <App pollInterval="2000" />,
     document.getElementById('example')
   );
-}, 500);
+
