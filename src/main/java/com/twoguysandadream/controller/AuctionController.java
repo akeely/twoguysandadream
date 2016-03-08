3aaf7c09c151a406a97f7b8b0908a7c1d998c430
Andrew Keely <andrew.keely@gmail.com>
2016-03-07 21:44:33 -0500

    Adding draft results

--- src/main/java/com/twoguysandadream/controller/AuctionController.java
+++ src/main/java/com/twoguysandadream/controller/AuctionController.java
@@ -6,18 +6,14 @@ import com.twoguysandadream.resources.MissingResourceException;
 import com.twoguysandadream.security.AuctionUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller
 public class AuctionController {
@@ -47,5 +43,42 @@ public class AuctionController {
         mav.addObject("league", league.get());
         mav.addObject("user", user);
         return mav;
+    }
+
+    @RequestMapping("/league/{leagueId}/results")
+    public ModelAndView draftResults(@PathVariable long leagueId, @AuthenticationPrincipal AuctionUser user)
+        throws MissingResourceException {
+
+        ModelAndView mav = new ModelAndView("draftResults");
+        League league = leagueRepository.findOne(leagueId)
+            .orElseThrow(() -> new MissingResourceException("league: " + leagueId));
+
+        List<WonPlayer> players = league.getRosters().entrySet().stream()
+            .flatMap((e) -> e.getValue().stream().map((p) -> new WonPlayer(e.getKey(), p)))
+            .collect(Collectors.toList());
+
+        mav.addObject("players", players);
+
+        return mav;
+    }
+
+    private static class WonPlayer {
+
+        private final Team team;
+        private final RosteredPlayer player;
+
+
+        private WonPlayer(Team team, RosteredPlayer player) {
+            this.team = team;
+            this.player = player;
+        }
+
+        public Team getTeam() {
+            return team;
+        }
+
+        public RosteredPlayer getPlayer() {
+            return player;
+        }
     }
 }
