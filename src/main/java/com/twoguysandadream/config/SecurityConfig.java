dbdf771ca80540016d1c092f4275b02e22b132a5
Andrew Keely <andrew.keely@gmail.com>
2016-03-29 21:31:23 -0400

    Disabling security for UAT

--- src/main/java/com/twoguysandadream/config/SecurityConfig.java
+++ src/main/java/com/twoguysandadream/config/SecurityConfig.java
@@ -5,12 +5,14 @@ import com.twoguysandadream.resources.ApiConfiguration;
 import com.twoguysandadream.security.OpenIdUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@Profile("!uat")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     static final String LOGIN_PAGE = "/login";
